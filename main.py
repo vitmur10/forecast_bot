0601391ac98e91b4681443efeefd4b4a1b13a24d
Vitaliy Murovaniy <95306614+vitmur10@users.noreply.github.com>
2022-06-06 23:05:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ def hello(message):
 @bot.message_handler(content_types=['text'])
 
 def prognoz(message):
-	m = str(message.text)
-	city = m[7:]
-	if 'погода' in m:
-		if len(m) > 6:
+	m2 = set(message.text)
+	city = message.text[7:]
+	if len(m2) == 1:
+		if 'z' in m2:
+			bot.send_message(message.chat.id, 'Вийди отсюда розбійник')
+		elif 'v' in m2:
+			bot.send_message(message.chat.id, 'Вийди отсюда розбійник')
+	elif len(m2) == 2:
+		if 'z' and 'v' in m2:
+			bot.send_message(message.chat.id, 'Вийди отсюда розбійник')
+
+	if "погода" in message.text:
+		if len(message.text) > 6:
 			try:
 				mgr = owm.weather_manager()
 				observation = mgr.weather_at_place(city)
@@ -27,15 +36,15 @@ def prognoz(message):
 				temp = w.temperature('celsius')['temp']
 				temp_max = w.temperature('celsius')['temp_max']
 				temp_min = w.temperature('celsius')['temp_min']
-				answre = f"""У {city} у даний момент {w.detailed_status}\nТемпература зараз {round(temp)}°С\nМакс.{round(temp_max)}°С\nМін.{round(temp_min)}°С"""
+				answre = f"""У {str.capitalize(city)} у даний момент {w.detailed_status}\nТемпература зараз {round(temp)}°С\nМакс.{round(temp_max)}°С\nМін.{round(temp_min)}°С"""
 				bot.send_message(message.chat.id, answre)
 				bot.register_next_step_handler(message, prognoz)
 			except:
 				bot.send_message(message.chat.id, 'Щось не так із містом \n'
 												  'Як правильно:\n'
 												  '(Погода місто)')
-	elif 'Погода' in m:
-		if len(m) > 6:
+	elif "погода" in message.text:
+		if len(message.text) > 6:
 			try:
 				mgr = owm.weather_manager()
 				observation = mgr.weather_at_place(city)
@@ -43,7 +52,7 @@ def prognoz(message):
 				temp = w.temperature('celsius')['temp']
 				temp_max = w.temperature('celsius')['temp_max']
 				temp_min = w.temperature('celsius')['temp_min']
-				answre = f"""У {city} у даний момент {w.detailed_status}\nТемпература зараз {round(temp)}°С\nМакс.{round(temp_max)}°С\nМін.{round(temp_min)}°С"""
+				answre = f"""У {str.capitalize(city)} у даний момент {w.detailed_status}\nТемпература зараз {temp}°С\nМакс.{temp_max}°С\nМін.{temp_min}°С"""
 				bot.send_message(message.chat.id, answre)
 				bot.register_next_step_handler(message, prognoz)
 			except:
@@ -52,3 +61,4 @@ def prognoz(message):
 												  '(Погода місто)')
 
 bot.polling(none_stop=True)
+
